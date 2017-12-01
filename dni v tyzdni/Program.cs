b56410f1f62bbec9a86d6d10bf2ea6cba6c03738
CSharpFri <CSharpFri@CLASS-2-11>
2017-12-01 20:08:03 +0100

    fin2

--- dni v tyzdni/Program.cs
+++ dni v tyzdni/Program.cs
@@ -10,8 +10,18 @@ namespace dni_v_tyzdni
     {
         static void Main(string[] args)
         {
-            dni_v_tyzdni.vypisdenvtyzdni("1");
-            dni_v_tyzdni.vypisdenvtyzdni("1",true);
+            //dni_v_tyzdni.vypisdenvtyzdni("1");
+            // dni_v_tyzdni.vypisdenvtyzdni("1",true);
+
+            //cykly.cykluswhile();
+
+            //cykly.Dowhile();
+
+            //cykly.cyklusfor();
+
+            //cykly.cyklusparameter(6);
+
+            cykly.cyklusforcontinue();
 
             Console.ReadKey();
         }
---
+++ dni v tyzdni/cykly.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dni_v_tyzdni
+{
+    class cykly
+    {
+        public static void cykluswhile()
+        {
+            int i =0;
+            while ( i < 4 )
+            {
+                Console.WriteLine("ahoj");
+                i++;
+            }
+        }
+        public static void Dowhile()
+        {
+            int i = 0;
+            do
+            {
+                i++;
+                Console.WriteLine(i);
+                
+            }
+            while (i <= 4);
+        }
+        public static void cyklusfor()
+        {
+            for (int i = 0,j = 10; i <= j; i++,j--)// i++ znamena i = i+1
+            {
+                Console.WriteLine("dvojice : {0},{1}",i,j);
+            }
+            
+        }
+        public static void cyklusparameter(int j)
+        {
+            for (int i = 0; i < j; i++)
+            {
+                Console.WriteLine("vypis");
+                if (i == 7) break;
+            }
+        }
+        public static void cyklusforcontinue()
+        {
+            for(int i = 0; i <= 10; i++)
+            {
+                int zbytok = i % 2;
+                if (zbytok != 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
